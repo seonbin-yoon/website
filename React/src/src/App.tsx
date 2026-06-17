00cd0912650d1a6c65e89582d2578bbf5755da3d
Seonbin Yoon <seonbin.yoon0@gmail.com>
2026-06-17 19:41:26 +0900

    first commit

---
+++ React/src/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+export default function Main() {
+  return (
+    <>
+      <div className='site'>
+        <div>
+          <p>홈</p>
+        </div>
+        <div className='under'>
+          <p className='copyright'>
+            2026 (c) 뿌댕이 All right reserved.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
